f4c0b265e26867dad8ccbec16ddc626f02008517
Hussain A <31271091+ceylonsullen@users.noreply.github.com>
2018-03-27 16:17:25 -0400

    Add files via upload

---
+++ first program/src/assignment_4_2_calculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class assignment_4_2_calculator {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		boolean flag;
+		While(flag)
+		System.out.println("Provide an expression");
+		String 
+
+	}
+
+}
---
+++ first program/src/assignment_4_2_coordinates_on_a_plane.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class assignment_4_2_coordinates_on_a_plane {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("max X?");
+		int max_X = input.nextInt();
+		System.out.println("max Y?");
+		int max_Y = input.nextInt();
+		int tempX = 0, tempY;
+		while(tempX <= max_X) {
+			tempY = 0;
+			while (tempY <= max_Y) {
+				System.out.println("(" + tempX + "," + tempY++ + ")");
+			}
+			tempX++;
+		}
+		input.close();
+	}
+
+}
